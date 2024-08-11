3a666fd9ad8e2000338027c02dbde04838702505
влад троян <loool.ke.k.6.6.6@gmail.com>
2024-08-11 22:13:10 +0200

    feature: Removed OAuth mapping. Refactoring.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'module/auth';
 import { PrismaService } from 'service/prisma';
-import { OAuthModule } from 'module/oauth';
 import { TwitchModule } from 'module/twitch';
 
 @Module({
   imports: [
-    OAuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env', '.env.local'],
